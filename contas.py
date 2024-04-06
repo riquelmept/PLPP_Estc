34e9bdd7f57faedd323d71a034a3908d35ca6cdd
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2024-04-06 14:27:37 -0300

    Add files via upload

---
+++ contas.py
@@ -0,0 +1,34 @@
+import datetime
+from extrato import Extrato
+
+class Conta:
+    def __init__(self,clientes,numero,saldo):
+        self.clientes = clientes
+        self.numero = numero
+        self.saldo = saldo
+        self.dataabertura = datetime.datetime.today()
+        self.extrato = Extrato () 
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.extrato.transacoes.append(["DEPOSITO", valor, "Data",datetime.datetime.today ()])
+
+    def sacar(self, valor):
+        if self.saldo < valor:
+            return False
+        else:
+            self.saldo -= valor
+            self.extrato.transacoes.append(["SAQUE", valor, "Data", datetime.datetime.today()]) 
+            return True
+
+    def transfereValor(self, contadestino, valor):
+        if self.saldo < valor:
+            return "Não existe saldo suficiente"
+        else:
+            contadestino.depositar(valor)
+            self.saldo -= valor
+            self.extrato.transacoes.append(["TRANSFERENCIA", valor, "Data", datetime.datetime.today()]) 
+            return "Transferencia Realizada"
+
+    def gerarsaldo(self):
+        print(f"numero: {self.numero}\n saldo:{self.saldo}")
